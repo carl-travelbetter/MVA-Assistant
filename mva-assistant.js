dd173485c7fbc1eba4d3c501d94d71a515fe6473
carl-travelbetter <cmciver@travelbetter.co.uk>
2025-10-04 14:10:30 +0100

    Update mva-assistant.js

--- mva-assistant.js
+++ mva-assistant.js
@@ -1,1 +1,9 @@
 console.log("MVA Assistant");
+
+const TAGS = {
+  week: [ "A", "B"],
+  subjects: ["English", "Maths", "Science", "History", "Geography"],
+  tasks: ["Prepare", "Share", "Develop", "Submit", "Build"]
+};
+
+
