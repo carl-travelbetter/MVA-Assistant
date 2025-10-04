40d0e1203a5ecb74bc51fdc96d678208bd81659a
carl-travelbetter <cmciver@travelbetter.co.uk>
2025-10-04 14:56:37 +0100

    Update mva-tracker.js

--- mva-tracker.js
+++ mva-tracker.js
@@ -11,9 +11,37 @@ console.log("Subject 2 "+TAGS.subjects[2]);
 //Load Tags
 const tagOptions = document.getElementById("tag-filters");
 
-TAGS.subjects.forEach(item => {
-  console.log("Subject "+item);
+//Load Week Buttons
+TAGS.week.forEach(item => {
   const tagButton = document.createElement("button");
+  tagButton.setAttribute("data-tag", item);
   tagButton.innerHTML = item;
+  tagOptions.appendChild(tagButton);
+});
+
+//Load Subject Buttons
+TAGS.subjects.forEach(item => {
+  const tagButton = document.createElement("button");
+  tagButton.setAttribute("data-tag", item);
+  tagButton.innerHTML = item;
+  tagOptions.appendChild(tagButton);
+});
+
+//Load Task Buttons
+TAGS.tasks.forEach(item => {
+  const tagButton = document.createElement("button");
+  tagButton.setAttribute("data-tag", item);
+  tagButton.innerHTML = item;
+
+  //Set listener so action can be taken
+  tagButton.addEventListener("click", () => {
+      tagButton.classList.toggle("active");
+      console.log("Clicked "+tagButton.dataset.tag");
+     // activeTags = Array.from(document.querySelectorAll('.tag-btn.active'))
+       // .map(btn => btn.dataset.tag);
+    
+      
+});
+  
   tagOptions.appendChild(tagButton);
 });
