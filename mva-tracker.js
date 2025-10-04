5f92832d70601ca5d88310f246a6a8eb7b2df5f4
carl-travelbetter <cmciver@travelbetter.co.uk>
2025-10-04 15:10:08 +0100

    Update mva-tracker.js

--- mva-tracker.js
+++ mva-tracker.js
@@ -11,25 +11,57 @@ console.log("Subject 2 "+TAGS.subjects[2]);
 //Load Tags
 const tagOptions = document.getElementById("tag-filters");
 
+//Set Week Tag Header
+const weekHeader = document.createElement("h3");
+weekHeader.textContent = "Select Week";
+tagOptions.appendChild("weekHeader");
+
 //Load Week Buttons
 TAGS.week.forEach(item => {
   const tagButton = document.createElement("button");
+  tagButton.className = "tag-btn";
   tagButton.setAttribute("data-tag", item);
   tagButton.innerHTML = item;
+  //Set listener so action can be taken
+  tagButton.addEventListener("click", () => {
+      tagButton.classList.toggle("active");
+      console.log("Clicked "+tagButton.dataset.tag);
+     // activeTags = Array.from(document.querySelectorAll('.tag-btn.active'))
+       // .map(btn => btn.dataset.tag);
+    });
   tagOptions.appendChild(tagButton);
 });
+
+//Set Subject Tag Header
+const subjectHeader = document.createElement("h3");
+subjectHeader.textContent = "Select Subjects";
+tagOptions.appendChild("subjectHeader");
 
 //Load Subject Buttons
 TAGS.subjects.forEach(item => {
   const tagButton = document.createElement("button");
+  tagButton.className = "tag-btn";
   tagButton.setAttribute("data-tag", item);
   tagButton.innerHTML = item;
+  //Set listener so action can be taken
+  tagButton.addEventListener("click", () => {
+      tagButton.classList.toggle("active");
+      console.log("Clicked "+tagButton.dataset.tag);
+     // activeTags = Array.from(document.querySelectorAll('.tag-btn.active'))
+       // .map(btn => btn.dataset.tag);
+    });
   tagOptions.appendChild(tagButton);
 });
+
+//Set Task Tag Header
+const taskHeader = document.createElement("h3");
+taskHeader.textContent = "Select Tasks";
+tagOptions.appendChild("taskHeader");
 
 //Load Task Buttons
 TAGS.tasks.forEach(item => {
   const tagButton = document.createElement("button");
+  tagButton.className = "tag-btn";
   tagButton.setAttribute("data-tag", item);
   tagButton.innerHTML = item;
 
@@ -39,9 +71,7 @@ TAGS.tasks.forEach(item => {
       console.log("Clicked "+tagButton.dataset.tag);
      // activeTags = Array.from(document.querySelectorAll('.tag-btn.active'))
        // .map(btn => btn.dataset.tag);
-    
-      
-});
+    });
   
   tagOptions.appendChild(tagButton);
 });
