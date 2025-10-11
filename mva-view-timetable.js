fb25e121dae50245ee53311b922e58da6000c594
carl-travelbetter <cmciver@travelbetter.co.uk>
2025-10-11 14:51:26 +0100

    Update mva-view-timetable.js

--- mva-view-timetable.js
+++ mva-view-timetable.js
@@ -1,9 +1,31 @@
+let weekDays = [];
+ fetch('week-days.json')
+  .then(response => response.json())
+  .then(data => {
+    weekDays = data;
+    console.log("Week Days Loaded:", weekDays);
+  })
+  .catch(error => console.error("Error loading week days", error));
+
 let timetable = [];
  fetch('timetable.json')
   .then(response => response.json())
   .then(data => {
     timetable = data;
     console.log("Time Table Loaded:", timetable);
-    loadTopThreeResults();
+    createControlBar();
   })
   .catch(error => console.error("Error loading timetable", error));
+
+function createControlBar()
+{
+ console.log("View Timetable - Create Control Bar");
+
+ //Create day buttons
+ const controlBar = document.getElementById("control-bar");
+ weekDays.forEach (week => {
+  const weekLabel = document.createElement("h2");
+  weekLabel.textContent = week;
+  controlBar.appendChild("weekLabel");
+ });
+}
