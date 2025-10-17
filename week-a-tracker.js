d942a2847ade22e7c1a94f7ace88656ae9b7ac16
carl-travelbetter <cmciver@travelbetter.co.uk>
2025-10-17 11:34:36 +0100

    Update week-a-tracker.js

--- week-a-tracker.js
+++ week-a-tracker.js
@@ -9,3 +9,34 @@ let slTimetable = [];
   })
   .catch(error => console.error("Error loading timetable", error));
 
+//load state data
+
+//Update the to do list when loading, when reset, or when an item is marked as not done
+function updateToDoList()
+{
+
+}
+
+//work out and show the next DL prioirty task
+function nextDLPriority()
+{
+
+}
+
+//Work out and show the next priority task
+function nextSLPriority()
+{
+ 
+} 
+
+//Update the done list when an item is marked as done
+function updateDoneList()
+{
+
+}
+
+//Reset the week
+function resetWeekA()
+{
+ 
+}
