074a3859fd12d3c380175232200732ecdf7a22a0
carl-travelbetter <cmciver@travelbetter.co.uk>
2025-10-26 22:02:02 +0000

    Update week-b-dbl-build-tracker.js

--- week-b-dbl-build-tracker.js
+++ week-b-dbl-build-tracker.js
@@ -10,6 +10,16 @@ let dblTimetable = [];
   })
   .catch(error => console.error("Error loading timetable", error));
 
+let daysLookup = [];
+ fetch('day-lookup.json')
+  .then(response => response.json())
+  .then(data => {
+    daysLookup = data;
+    console.log("Days Look Up Loaded", daysLookup);
+    console.log("Days Look Up "+daysLookup.length);
+  })
+  .catch(error => console.error("Error loading days lookup", error));
+
 const SLTAGS = {
   subjects: ["English Language", "Maths"]
 };
@@ -169,7 +179,7 @@ function getNextTask()
         subject.textContent = item.subject+" Build Tasks";
         output.appendChild(subject);
         const dueDay = document.createElement("p");
-        dueDay.textContent = "Due by day "+item.day;
+        dueDay.textContent = "Due by day "+lookupDay(item.day+1);
         output.appendChild(dueDay);
         const doneButton = document.createElement("button");
         doneButton.className = "control-btn";
@@ -219,6 +229,22 @@ function updateDoneList()
   }
   
  });
+}
+
+function lookupDay(number)
+{
+   console.log("Look up day "+number);
+   let response = "Day Not Found";
+   daysLookup.forEach(item => {
+    if (item.day == number)
+    {
+      console.log("Match Found");
+      response = item.label;
+    }
+   
+   });
+   
+   return response;
 }
 
 //Reset the week
