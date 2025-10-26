3eaf1065d05a8ea064464804505b6a96871d9ad5
carl-travelbetter <cmciver@travelbetter.co.uk>
2025-10-26 22:56:21 +0000

    Update mva-tracking-dashboard.js

--- mva-tracking-dashboard.js
+++ mva-tracking-dashboard.js
@@ -6,6 +6,7 @@ weekADevelopStats();
 weekADblPrepStats();
 weekADblBuildStats();
 weekADblSubmitStats();
+weekBPrepStats();
 
 function weekAPrepStats()
 {
@@ -171,6 +172,34 @@ function weekADblSubmitStats()
     const stats = document.createElement("a");
     stats.href = "week-a-dbl-submit.html";
     stats.textContent = done+" Double Submit Items Done "+toDo+" Still to Do";
+    statP.appendChild(stats);
+    statsCont.appendChild(statP);
+}
+
+function weekBPrepStats()
+{
+    const STORAGE_KEY = "mva_b_prep_sl_tracker";
+    let state = JSON.parse(localStorage.getItem(STORAGE_KEY)) || {position: []};
+    
+    let done = 0;
+    let toDo = 0;
+    
+    state.position.forEach(item => {
+      if (item.status === "Done")
+      {
+        done++;
+      }
+      else
+      {
+        toDo++;
+      }
+    });
+    
+    const statsCont = document.getElementById("week-a-dbl-prep");
+    const statP = document.createElement("p");
+    const stats = document.createElement("a");
+    stats.href = "week-a-sl-prep.html";
+    stats.textContent = done+" Double Lesson Prep Items Done "+toDo+" Still to Do";
     statP.appendChild(stats);
     statsCont.appendChild(statP);
 }
