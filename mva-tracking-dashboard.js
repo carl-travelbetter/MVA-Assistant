fdfc64ad4b834b59c5c2f967b589213af7a3b52a
carl-travelbetter <cmciver@travelbetter.co.uk>
2025-10-26 22:48:10 +0000

    Update mva-tracking-dashboard.js

--- mva-tracking-dashboard.js
+++ mva-tracking-dashboard.js
@@ -2,6 +2,7 @@ console.log("MVA Tracking Dashboard");
 
 weekAPrepStats();
 weekABuildStats();
+weekADevelopStats()
 
 function weekAPrepStats()
 {
@@ -26,7 +27,7 @@ function weekAPrepStats()
     const statP = document.createElement("p");
     const stats = document.createElement("a");
     stats.href = "week-a-sl-prep.html";
-    stats.textContent = done+" Prep Items Done "+toDo+" Still to Do";
+    stats.textContent = done+" Single Lesson Prep Items Done "+toDo+" Still to Do";
     statP.appendChild(stats);
     statsCont.appendChild(statP);
 }
@@ -54,7 +55,35 @@ function weekABuildStats()
     const statP = document.createElement("p");
     const stats = document.createElement("a");
     stats.href = "week-a-sl-build.html";
-    stats.textContent = done+" Build Items Done "+toDo+" Still to Do";
+    stats.textContent = done+" Single Build Items Done "+toDo+" Still to Do";
+    statP.appendChild(stats);
+    statsCont.appendChild(statP);
+}
+
+function weekADevelopStats()
+{
+    const STORAGE_KEY = "mva_a_sldevelop_tracker";
+    let state = JSON.parse(localStorage.getItem(STORAGE_KEY)) || {position: []};
+    
+    let done = 0;
+    let toDo = 0;
+    
+    state.position.forEach(item => {
+      if (item.status === "Done")
+      {
+        done++;
+      }
+      else
+      {
+        toDo++;
+      }
+    });
+    
+    const statsCont = document.getElementById("week-a-sl-Develop");
+    const statP = document.createElement("p");
+    const stats = document.createElement("a");
+    stats.href = "week-a-sl-build.html";
+    stats.textContent = done+" Single Develop Items Done "+toDo+" Still to Do";
     statP.appendChild(stats);
     statsCont.appendChild(statP);
 }
