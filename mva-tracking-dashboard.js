cbbdac2359615b8657cab22d460f45210d278180
carl-travelbetter <cmciver@travelbetter.co.uk>
2025-11-09 14:13:54 +0000

    Update mva-tracking-dashboard.js

--- mva-tracking-dashboard.js
+++ mva-tracking-dashboard.js
@@ -8,6 +8,10 @@ weekADblBuildStats();
 weekADblSubmitStats();
 weekBPrepStats();
 weekBSubmitStats();
+weekBDevelopStats();
+weekBDblPrepStats();
+weekBDblBuildStats();
+weekBDblDevelopStats();
 
 function weekAPrepStats()
 {
@@ -229,6 +233,122 @@ function weekBSubmitStats()
     const stats = document.createElement("a");
     stats.href = "week-b-sl-submit.html";
     stats.textContent = done+" Single Lesson Submit Items Done "+toDo+" Still to Do";
+    statP.appendChild(stats);
+    statsCont.appendChild(statP);
+
+}
+
+function weekBDevelopStats()
+{
+    const STORAGE_KEY = "mva_b_sldevelop_tracker";
+    let state = JSON.parse(localStorage.getItem(STORAGE_KEY)) || {position: []};
+    
+    let done = 0;
+    let toDo = 0;
+    
+    state.position.forEach(item => {
+      if (item.status === "Done")
+      {
+        done++;
+      }
+      else
+      {
+        toDo++;
+      }
+    });
+    
+    const statsCont = document.getElementById("week-b-sl-develop");
+    const statP = document.createElement("p");
+    const stats = document.createElement("a");
+    stats.href = "week-b-sl-develop.html";
+    stats.textContent = done+" Single Lesson Develop Items Done "+toDo+" Still to Do";
+    statP.appendChild(stats);
+    statsCont.appendChild(statP);
+
+}
+
+function weekBDblPrepStats()
+{
+    const STORAGE_KEY = "mva_b_dbl_prep_tracker";
+    let state = JSON.parse(localStorage.getItem(STORAGE_KEY)) || {position: []};
+    
+    let done = 0;
+    let toDo = 0;
+    
+    state.position.forEach(item => {
+      if (item.status === "Done")
+      {
+        done++;
+      }
+      else
+      {
+        toDo++;
+      }
+    });
+    
+    const statsCont = document.getElementById("week-b-dbl-prep");
+    const statP = document.createElement("p");
+    const stats = document.createElement("a");
+    stats.href = "week-b-dbl-prep.html";
+    stats.textContent = done+" Double Lesson Prep Items Done "+toDo+" Still to Do";
+    statP.appendChild(stats);
+    statsCont.appendChild(statP);
+
+}
+
+function weekBDblBuildStats()
+{
+    const STORAGE_KEY = "mva_b_dbl_build_tracker";
+    let state = JSON.parse(localStorage.getItem(STORAGE_KEY)) || {position: []};
+    
+    let done = 0;
+    let toDo = 0;
+    
+    state.position.forEach(item => {
+      if (item.status === "Done")
+      {
+        done++;
+      }
+      else
+      {
+        toDo++;
+      }
+    });
+    
+    const statsCont = document.getElementById("week-b-dbl-build");
+    const statP = document.createElement("p");
+    const stats = document.createElement("a");
+    stats.href = "week-b-dbl-build.html";
+    stats.textContent = done+" Double Lesson Build Items Done "+toDo+" Still to Do";
+    statP.appendChild(stats);
+    statsCont.appendChild(statP);
+
+}
+
+function weekBDblDevelopStats()
+{
+    const STORAGE_KEY = "mva_b_dbl_develop_tracker";
+    let state = JSON.parse(localStorage.getItem(STORAGE_KEY)) || {position: []};
+    
+    let done = 0;
+    let toDo = 0;
+    
+    state.position.forEach(item => {
+      if (item.status === "Done")
+      {
+        done++;
+      }
+      else
+      {
+        toDo++;
+      }
+    });
+    
+    const statsCont = document.getElementById("week-b-dbl-develop");
+    const statP = document.createElement("p");
+    const stats = document.createElement("a");
+    stats.href = "week-b-dbl-develop.html";
+    stats.textContent = done+" Double Lesson Develop Items Done "+toDo+" Still to Do";
     statP.appendChild(stats);
     statsCont.appendChild(statP);
 
